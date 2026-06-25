3511b399481e9720d1a502dc5f0a82e1dc4c86e5
Cris Stanza <crisstanza@users.noreply.github.com>
2026-06-25 20:42:50 -0300

    test bucket (aula 3)

---
+++ src/main/java/bucket/demo/JavaDemoBucketApplication.java
@@ -0,0 +1,13 @@
+package bucket.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDemoBucketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDemoBucketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bucket/demo/JavaDemoBucketApplicationTests.java
@@ -0,0 +1,13 @@
+package bucket.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDemoBucketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
