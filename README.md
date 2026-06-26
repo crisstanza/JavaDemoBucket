63bba95d5d0d6bfd106a32f839e2a8479f03e20e
Cris Stanza <crisstanza@users.noreply.github.com>
2026-06-25 22:28:40 -0300

    more tests

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # JavaDemoBucket
+
+http://18.230.58.42:8080/dynamodb/scan
--- scripts/test.bash
+++ scripts/test.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
-curl -X POST http://18.230.58.42:8080/dynamodb/save?id=two\&nome=lorem-ipsum
+curl -X POST http://18.230.58.42:8080/dynamodb/save?id=RUMMMMM\&nome=
 echo ''
 curl -X GET http://18.230.58.42:8080/dynamodb/scan
+echo ''
